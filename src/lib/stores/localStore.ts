74ddd36fd143fb62f6307db5c08cdf9e806fae3e
gzuuus <gzuuus@protonmail.com>
2024-02-23 13:02:23 +0000

    favicon, readme

--- src/lib/stores/localStore.ts
+++ src/lib/stores/localStore.ts
@@ -1,7 +1,6 @@
 import type { UserSettings } from '$lib/interfaces';
 import { localStorageStore } from '@skeletonlabs/skeleton';
 import type { Writable } from 'svelte/store';
-// export const rTopics: Writable<string[]> = localStorageStore('r/', ['Music', 'General', 'random']);
 
 export const appSetings: Writable<UserSettings> = localStorageStore('UserSettings', {
 	lastUserLogged: undefined,
